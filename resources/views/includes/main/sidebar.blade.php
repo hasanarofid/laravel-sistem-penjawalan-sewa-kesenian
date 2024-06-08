5bde07f813bb970368b10325851d224d9857a8cf
hasanarofid <hasanarofid@gmail.com>
2024-06-08 22:12:25 +0700

    db jasa seni

--- resources/views/includes/main/sidebar.blade.php
+++ resources/views/includes/main/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="index.html">ROOMING</a>
+      <a href="index.html">SIM Penjadwalan</a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
       <a href="index.html">RM</a>
