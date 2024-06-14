e07348370f881fb493e35fece44a2adce7f55786
hasanarofid <hasanarofid@gmail.com>
2024-06-14 13:57:55 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,31 @@
 @extends('layouts.app')
 
-@section('title', 'Login - ROOMING')
+@section('title', 'Login - Sanggar Seni Putra Karuhun')
 
 @section('content')
+<style>
+  body {
+    background: url('/kesenian.jpg') no-repeat center center fixed;
+    background-size: cover;
+}
+
+.login-brand {
+    font-size: 2em;
+    margin-bottom: 20px;
+    text-align: center;
+    color: #fff; /* Optional: Change this according to your theme */
+}
+
+.card-primary {
+    background-color: rgba(255, 255, 255, 0.9); /* Make the card background slightly transparent */
+}
+</style>
 <section class="section">
     <div class="container mt-5">
       <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-          <div class="login-brand">
-            SIM <br> Penjadwalan dan Penyewaan Sasa Kesenian
+          <div class="login-brand" style="color: black">
+            Sanggar Seni Putra Karuhun
           </div>
 
           <div class="card card-primary">
@@ -49,8 +66,8 @@
             </div>
           </div>
           
-          <div class="simple-footer">
-            Copyright &copy; Penjadwalan dan Penyewaan Sasa Kesenian @ {{ date('Y') }}
+          <div class="simple-footer" style="color: black">
+            Copyright &copy; Sanggar Seni Putra Karuhun @ {{ date('Y') }}
           </div>
         </div>
       </div>
